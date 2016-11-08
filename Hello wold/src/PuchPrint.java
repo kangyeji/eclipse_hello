02feaaa2c306fa0111e88f1737b5cb5627fbdfa7
kangyeji <yejik0105@naver.com>
2016-11-08 15:16:15 +0900

    remote Pushprint.java modified

--- Hello wold/src/PuchPrint.java
+++ Hello wold/src/PuchPrint.java
@@ -4,7 +4,8 @@ public class PuchPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
 		System.out.println("Add java Class File Push");	
-		System.out.println("word with 360");			
+		System.out.println("word with 360");
+		System.out.println("remote master");
 	}
 
 }
