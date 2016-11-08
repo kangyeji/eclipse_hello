32d3059864b015b78ae449abb952997128fbcf0a
kangyeji <yejik0105@naver.com>
2016-11-08 14:52:29 +0900

    pushprint.java modified by github

--- Hello wold/src/PuchPrint.java
+++ Hello wold/src/PuchPrint.java
@@ -2,7 +2,9 @@
 public class PuchPrint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub		System.out.println("Add java Class File Push");		St		
+		// TODO Auto-generated method stub		
+		System.out.println("Add java Class File Push");	
+		System.out.println("word with 360");			
 	}
 
 }
